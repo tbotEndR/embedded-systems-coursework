a9c9fcd4c2d10678d553e5b1f99aa8b2e19d6e74
tbotEndR <thibaut.endress27@proton.me>
2024-10-22 16:17:28 +0200

    ueb_2/a1 - ported previous program to use interrupts to register button presses

--- ueb_2/a1/main.c
+++ ueb_2/a1/main.c
@@ -1,76 +1,104 @@
 #define F_CPU 16000000L
+#define TIMERSTART_F 2
+#define TIMERRESET_F 3
+#define DIPLIMIT_REG 4
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
 /*
-PORT MAPPING: 
-PORTD3: LED1 | PORTD4: LED2 | PORTD5: LED3
-PIND6: DIP1 | PIND7: DIP2 | PINB0: DIP3 | PINB1: RESET | PIND2: START_Interrupt 
+- - - INPUTS - - - 
+PIND2: START (INT0)
+PIND6: DIP1 
+PIND7: DIP2
+PINB0: RESET (PCINT0) 
+PINB1: DIP3
+
+- - - OUTPUTS - - -
+** Ascending right to left **
+PORTD3: LED1
+PORTD4: LED2
+PORTD5: LED3
 */
 
-volatile int8_t counter = 7, dipInput = 7, tmp = 0x0, msTimer = 0;
-volatile char startButtonDown = '0', countdown = '1', resetButtonDown = '0', reset = '0';
+uint8_t flags = 0x00;
+/* 	
+    Bit2: TimerStarted | Bit3: TimerReset  
+    Bits 4-7: User Input Limit
+*/
 
-// start
 ISR(INT0_vect)
 {
-	countdown = '1';
-	reset = '0';
+    /*	Start Button Logic:
+     *	- set TimerStarted flag		*/
+
+    flags |= (1<<TIMERSTART_F);
+    flags &= ~(1<<TIMERRESET_F);
 }
 
-// reset
-ISR(PCINT1_vect)
+ISR(PCINT0_vect)
 {
-	reset = '1';
-	countdown = '0';
+    /*	Reset Button Logic:
+     *	- set TimerReset flag		*/
+
+	flags |=  (1<<TIMERRESET_F);
+	flags &= ~(1<<TIMERSTART_F);
 }
+
 
 int main(void)
 {
-	// init LEDs to hi
-	DDRD |= (1<<DDD3) | (1<<DDD4) | (1<<DDD5);
-	PORTD |= (1<<PORTD3) | (1<<PORTD4) | (1<<PORTD5);
-	// set pullups for inputs
-	PORTD |= (1<<PORTD2) | (1<<PORTD6) | (1<<PORTD7);
-	PORTB |= (1<<PORTB0) | (1<<PORTB1);
-	PORTC |= (1<<PORTC2);
-	// configure interrupt rising edge detection
-	PCICR &= ~(1<<PCIE1);
-	PCICR |= (1<<PCIE1);
-	EICRA &= ~((1<<ISC01) | (1<<ISC00));
-	EICRA |= (1<<ISC01);
-	// enable INT0 interrupt
-	EIMSK |= (1<<INT0);
-	// enable PCINT1 interrupt
-	PCMSK1 |= (1<<PCINT10);
-	// enable interrupts beside reset
-	sei();
+    // OUTPUTS
+    DDRD |= (1<<DDD3) | (1<<DDD4) | (1<<DDD5);
+    PORTD |= (1<<PORTD3) | (1<<PORTD4) | (1<<PORTD5);
+    // INPUTS
+    PORTD |= (1<<PORTD2) | (1<<PORTD6) | (1<<PORTD7);
+    PORTB |= (1<<PORTB0) | (1<<PORTB1);
+    /*	Interrupt setup */
+    EICRA &= ~((1<<ISC01) | (1<<ISC00));
+    EICRA |= (1<<ISC01);
+    EIMSK |= (1<<INT0);
+    PCICR &= ~(1<<PCIE0);
+    PCICR |= (1<<PCIE0);
+    PCMSK0 |= (1<<PCINT0);
+    sei();
 
-	while(1)
+    int8_t countdown = 0x07, loop_counter = 0x00;
+    flags |= (countdown << DIPLIMIT_REG);
+
+    while(1)
+    {
+	/*	Timer Started State:
+	 *	- output countdown bits on leds 1-3
+	 * 	- decrement countdown every 1s		*/
+
+	if (flags & (1<<TIMERSTART_F))
 	{
-		if (reset == '1')
-		{
-			msTimer = 0;
-			dipInput = ((PIND&(1<<PIND6))>>4 | (PIND&(1<<PIND7))>>6 | (PINB&(1<<PINB0)));
-			counter = dipInput;
-			tmp = ((PORTD & (~(0b111<<PORTD3))) | (~counter&0x07)<<PORTD3);
-			PORTD = tmp;
-		}
-
-		if ((msTimer >= 10) && (countdown == '1'))
-		{
-			if (counter < 0)
-			{
-				counter = dipInput;
-			}
-			PORTD &= ~((1<<PORTD3) | (1<<PORTD4) | (1<<PORTD5));	// reset LED pins
-			PORTD |= (0xF & ~counter)<<PORTD3;
-			counter--;
-			msTimer = 0;
-		}
-		
-		msTimer++;
-		_delay_ms(100);		
+	    if (loop_counter >= 10)
+	    {
+		if (countdown < 0)  countdown = (flags >> DIPLIMIT_REG);
+		PORTD &= ~(0x7 << PORTD3);
+		PORTD |= (0x07 & ~countdown) << PORTD3;
+		countdown--;
+		loop_counter = 0;
+	    }
 	}
+
+	/*	Timer Reset State:
+	 *	- read user input from dip switches 1-3
+	 *	- save user input as new countdown limit		*/
+
+	if (flags & (1<<TIMERRESET_F))
+	{
+	    countdown = ~((PIND & (1<<PIND6))>>4 | (PIND & (1<<PIND7))>>6 | (PINB & (1<<PINB1))>>1) & 0x07;
+	    flags |= (0x0F << DIPLIMIT_REG);
+	    flags &= (countdown << DIPLIMIT_REG) | 0x0F;
+	    countdown = (flags >> DIPLIMIT_REG) & 0x07;
+	    PORTD &= ~(0x7 << PORTD3);
+	    PORTD |= (~countdown << PORTD3);
+	}
+	loop_counter++;
+	_delay_ms(100);
+
+    }
 }
